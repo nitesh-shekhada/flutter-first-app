247c2fba78d300ea6c32a06e4fa53af767c56ccf
nitesh-shekhada <niteshshekhada33@gmail.com>
2021-05-07 11:38:52 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Hello World",
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            brightness: Brightness.light,
+            accentColor: Colors.blue),
+        home: new HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String mytext = "Hello";
+  void _changeText() {
+    setState(() {
+      if (mytext.startsWith("H")) {
+        mytext = "My App";
+      } else {
+        mytext = "Hello";
+      }
+    });
+  }
+
+  Widget _bodyWidget() {
+    return new Container(
+      padding: const EdgeInsets.all(8.0),
+      child: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            new Text(mytext),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+      ),
+      body: _bodyWidget(),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.add),
+        onPressed: _changeText,
+      ),
+    );
+  }
+}
